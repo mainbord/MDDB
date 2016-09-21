34d26e7db8beb4fe11030abd1e6c15be082c757e
mainbord <n56016gb@gmail.com>
2016-09-22 01:22:02 +0300

    Add interaction with Database

--- mddb/StartForm.java
+++ mddb/StartForm.java
@@ -1,9 +1,11 @@
-package com.MDDB;
+package com.mddb;
 
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by work on 20.09.2016.
@@ -11,43 +13,51 @@ import java.util.ArrayList;
 public class StartForm {
     private JPanel panelMain;
 
-    StartForm(String [][] rows){
+    StartForm(Map<String,String> raws){
         panelMain = new JPanel();
-        panelMain.setLayout(new GridLayout(rows.length,rows[0].length));
-        ArrayList<JTextArea> listOfTextFields = new ArrayList<>();
+        panelMain.setLayout(new GridLayout(raws.keySet().size(),2));
 
-        for (int i = 0; i < rows.length; i++) {
-            for (int j = 0; j < rows[0].length; j++) {
-                JTextArea jtext = new JTextArea(rows[i][j]);
-                if (j%2==0){
-                    jtext.setFont(new Font(jtext.getFont().getName(), Font.BOLD, 18));
-                }
-                panelMain.add(jtext);
-            }
+        for (Map.Entry<String,String> temp : raws.entrySet()){
+            JTextArea jtext = new JTextArea(temp.getKey());
+            jtext.setFont(new Font(jtext.getFont().getName(), Font.BOLD, 16));
+            panelMain.add(jtext);
+            jtext = new JTextArea(temp.getValue());
+            panelMain.add(jtext);
         }
     }
 
     public static void main(String[] args) {
+
+        String [][] params = new String[10][2];
+        Map<String,String> devicesParamsMap = new HashMap<>();
+
         Connection c = null;
         Statement stmt = null;
         try {
             //Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:MDDB.mddb");
+            //To use DriverManager you need sqlite driver in /jre/lib/ext
+            c = DriverManager.getConnection("jdbc:sqlite:/home/mainbord/Документы/MDDB");
+
             System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
-            String sql = "SELECT * FROM Devices";
-            //stmt.executeUpdate(sql);
-
-            //System.out.printf(stmt.getResultSet().toString());
-
+            String sql = "SELECT * FROM Devices WHERE Company='Motorola'";
             ResultSet rs = stmt.executeQuery(sql);
             ResultSetMetaData rsmd = rs.getMetaData();
-            String name = rsmd.getColumnName(1);
-            System.out.println(name);
-            while(rs.next()){
-                System.out.println(rs.getString("Company"));
+            ArrayList<String> listColumnsNames = new ArrayList<>();
+            // Recieving all names of all colums of this table
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                String name = rsmd.getColumnName(i);
+                listColumnsNames.add(name);
             }
+            //printing all data from the table
+            while(rs.next()){
+                for (String columnName : listColumnsNames){
+                    devicesParamsMap.put(columnName,rs.getString(columnName));
+                }
+            }
+
+            params = new String[listColumnsNames.size()][2];
 
             stmt.close();
             c.close();
@@ -58,37 +68,14 @@ public class StartForm {
         System.out.println("SQL Script sucessfully");
 
 
-
-
-
         JFrame frame = new JFrame("Mainbord Device Data Base");
-
         //Здесь достаём из БД нужный телефон с характеристиками [строк][столбцов]
-        String [][] params = new String[10][2];
-        params[0][0] = "Операционная система";
-        params[0][1] = "Неизвестно";
-        params[1][0] = "Плата";
-        params[1][1] = "Неизвестно";
-        params[2][0] = "Центральный процессор";
-        params[2][1] = "Неизвестно";
-        params[3][0] = "Графический ускоритель";
-        params[3][1] = "Неизвестно";
-        params[4][0] = "Оперативная память (ОЗУ, RAM)";
-        params[4][1] = "Неизвестно";
-        params[5][0] = "Встроенный накопитель (ПЗУ, ROM)";
-        params[5][1] = "Неизвестно";
-        params[6][0] = "Аудиочип";
-        params[6][1] = "Неизвестно";
-        params[7][0] = "Аккумулятор";
-        params[7][1] = "Неизвестно";
-        params[8][0] = "Форм-фактор";
-        params[8][1] = "Неизвестно";
-        params[9][0] = "Размеры";
-        params[9][1] = "Неизвестно";
 
-        frame.setContentPane(new StartForm(params).panelMain);
+
+        frame.setContentPane(new StartForm(devicesParamsMap).panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setSize(600,600);
         frame.setVisible(true);
     }
 
